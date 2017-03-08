08778235bcdac494506f41df4e6cd11f254692e3
Scott Bromander <scott@primeacademy.io>
2017-03-07 21:15:30 -0600

    initial commit

---
+++ client.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $(".kittyCat").text("ChiYaks are awesome!");
+    $(".container").append("<p>Phi is 'ok'</p>");
+    $(".container").append("<p>Here is another p tag</p>");
+});
